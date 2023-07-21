5302c61c4ad622baec465ff1fd2fcdcec6e89b98
akramfirmansyah <akram.firman@gmail.com>
2023-07-22 03:14:57 +0800

    add section about and contact

--- app/themes.tsx
+++ app/themes.tsx
@@ -1,11 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+  primary: {
+    blue: "#6919FF",
+    red: "#FF2E5B",
+    black: "#060918",
+    grey: "#7F939E",
   },
 };
 
-export const theme = extendTheme({ colors });
+export const theme = extendTheme({
+  colors,
+});
